8e3d2a74d48ce5b6800557749a6e1649fdef71f1
Reviva <102226233+RevivaECE@users.noreply.github.com>
2022-03-23 18:27:16 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+function menuFunction(){
+    var x = document.getElementById("menu-clickk");
+    if(x.style.display != "block")
+    {
+        x.style.display = "block";
+    }
+    else{
+        x.style.display="none";
+    }
+}
